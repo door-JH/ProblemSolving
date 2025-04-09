edf246ec21efb4355031ea450bf39728a4de2ce7
door JH <elogio.mi@gmail.com>
2025-04-09 19:07:59 +0900

    [FEAT] P120830, P120831

---
+++ Programmers/beginner/P120830.java
@@ -0,0 +1,10 @@
+package Programmers.beginner;
+
+public class P120830 {
+    public int solution(int n, int k) {
+        return (n * 12000) + (k * 2000) - (n/10 * 2000);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Programmers/beginner/P120831.java
@@ -0,0 +1,14 @@
+package Programmers.beginner;
+
+import java.util.stream.IntStream;
+
+public class P120831 {
+    public int solution(int n) {
+        return IntStream.range(0, n+1)
+                    .filter(num -> num % 2 == 0)
+                    .sum();
+    }
+    public static void main(String[] args) {
+        
+    }
+}
