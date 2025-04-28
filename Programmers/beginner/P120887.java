85adde4544a37e83ab76f371a49d02de7f8e749e
door JH <elogio.mi@gmail.com>
2025-04-28 23:51:29 +0900

    [FIX] P120887

---
+++ Programmers/beginner/P120887.java
@@ -0,0 +1,23 @@
+package Programmers.beginner;
+
+class P120887{
+
+    public static int solution(int i, int j, int k) {
+        int answer = 0;
+        String strNum = "";
+        for(int q = i; q <= j; q++){
+            strNum = Integer.toString(q);
+            for(int w = 0; w < strNum.length(); w++){
+                if(strNum.charAt(w) == Integer.toString(k).charAt(0)) answer++;
+            }
+        }
+        
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        solution(1, 13, 1);
+
+    }
+}
