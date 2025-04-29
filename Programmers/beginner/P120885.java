615ff465289e5e41f6d680153d56779fb66ea7e6
door JH <elogio.mi@gmail.com>
2025-04-29 23:49:59 +0900

    [FEAT] P120885 풀이중

---
+++ Programmers/beginner/P120885.java
@@ -0,0 +1,24 @@
+package Programmers.beginner;
+
+public class P120885 {
+
+    public String solution(String bin1, String bin2) {
+        String answer = "";
+        int loop = bin1.length() > bin2.length() ? bin1.length() : bin2.length();
+        boolean isUpper = false;
+
+
+        for(int q = loop; q >= 0; q--){
+
+            
+
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
