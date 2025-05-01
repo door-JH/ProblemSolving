0d528374312be42e7c1545f6b190ae744acf5874
door JH <elogio.mi@gmail.com>
2025-05-01 23:32:51 +0900

    [FEAT] P120883

---
+++ Programmers/beginner/P120883.java
@@ -0,0 +1,13 @@
+package Programmers.beginner;
+
+public class P120883 {
+
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
