64cb75dac97f33a6a9879f6cefc59bc8a5659ab2
door JH <elogio.mi@gmail.com>
2025-05-02 23:52:13 +0900

    [FIX] P120883

--- Programmers/beginner/P120883.java
+++ Programmers/beginner/P120883.java
@@ -1,9 +1,22 @@
 package Programmers.beginner;
 
+
 public class P120883 {
 
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
+        String answer = "fail";
+    
+
+        for(int q = 0; q < db.length; q++){
+            if(id_pw[0].equals(db[q][0])){
+                answer = "wrong pw";
+                if (id_pw[1].equals(db[q][1])) {
+                    answer = "login";
+                }
+            }
+        }
+
+
         return answer;
     }
 
