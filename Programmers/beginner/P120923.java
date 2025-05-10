8c99c30780ed018ce57d3eeb7e8bb1aec94fe982
door JH <elogio.mi@gmail.com>
2025-05-11 02:16:36 +0900

    [FEAT] P120923

---
+++ Programmers/beginner/P120923.java
@@ -0,0 +1,31 @@
+package Programmers.beginner;
+
+import java.util.Arrays;
+
+public class P120923 {
+    public static int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int sum = 1000;
+        int cnt = -1;
+        // 입력값 〉 5, 0
+        // 기댓값 〉 [-2, -1, 0, 1, 2]
+        while(sum != total){
+            sum = 0;
+            cnt++;
+            for(int i = 1; i <= num; i++ ){
+                sum += (1000 - cnt - i);
+                answer[i - 1] = (1000 - cnt - i);
+            }
+            
+        }
+        Arrays.sort(answer);
+        for (int i : answer) {
+            System.out.println(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(5, 0);
+    }
+}
