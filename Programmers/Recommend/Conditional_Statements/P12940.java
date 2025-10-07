271d0f6350a9ecf16c268389ddf62478966428c6
door JH <elogio.mi@gmail.com>
2025-10-07 23:43:08 +0900

    [FEAT] P12940

---
+++ Programmers/Recommend/Conditional_Statements/P12940.java
@@ -0,0 +1,28 @@
+package Programmers.Recommend.temp;
+
+public class P12940 {
+    public int GCD(int n, int m){
+        int temp = 1;
+        while (m != 0){
+            temp = m;
+            m = n % m;
+            n = temp;
+        }
+        return n;
+    }
+    
+    public int LCM(int n, int m){
+        return n * m / GCD(n, m);
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = GCD(n, m);
+        answer[1] = LCM(n, m);
+        
+        
+        
+        return answer;
+    }
+}
