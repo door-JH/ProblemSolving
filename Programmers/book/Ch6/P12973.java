ab7ef91516bc2a626bb96bd9a6fd779e1aecee89
door JH <elogio.mi@gmail.com>
2025-10-28 21:52:00 +0900

    [FEAT] P12973, P42584, P76502 추가

---
+++ Programmers/book/Ch6/P12973.java
@@ -0,0 +1,23 @@
+package Programmers.book.Ch6;
+
+import java.util.ArrayDeque;
+
+public class P12973 {
+    public int solution(String s)
+    {   
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+       
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
